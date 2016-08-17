943abd1533e16107f26433f68ccbd8e6b5654f9c
Suhas-Kopparapu <kopparapu.suhas@gmail.com>
2016-08-17 19:09:10 +0530

    Assignment3
    Wrong data declaration

---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+package Assignments;
+
+public class Assignment3 {
+	public static void main(String[] args){
+		//initializing variables
+		int a;
+		short c;
+		//declaring wrong values to variables
+		a=10.555864321;
+		c=200000;
+	}
+
+}
